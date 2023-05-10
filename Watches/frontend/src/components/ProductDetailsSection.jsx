e3ebb3c567608a4f177c3dc7b0ad59d3234bb8ae
Carl Hammarling <carlhammarling@gmail.com>
2023-05-10 19:01:29 +0100

    Working on prodDescription

--- Watches/frontend/src/components/ProductDetailsSection.jsx
+++ Watches/frontend/src/components/ProductDetailsSection.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const ProductDetailsSection = () => {
   return (
-    <div>ProductDetailsSection</div>
+    <div className='productDetailsSection'>
+      <div className="row">
+        <div className="leftImages">
+          <div className="bigImg">
+            
+          </div>
+          <div className="imgSnapCarousel">
+            
+          </div>
+
+        </div>
+        <div className="rightDescription">
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
