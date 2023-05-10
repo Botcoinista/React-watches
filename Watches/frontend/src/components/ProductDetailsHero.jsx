f5f7a1cd4823b191cc6e207c7037b8d89c9b8458
Carl Hammarling <carlhammarling@gmail.com>
2023-05-10 11:15:31 +0100

    Working on ProdDetails

--- Watches/frontend/src/components/ProductDetailsHero.jsx
+++ Watches/frontend/src/components/ProductDetailsHero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProductDetailsHero = () => {
   return (
-    <div>ProductDetailsHero</div>
+    <div className='productDetailsHero'>ProductDetailsHero</div>
   )
 }
 
