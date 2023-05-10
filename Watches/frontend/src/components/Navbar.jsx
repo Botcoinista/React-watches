5d2d02e352b4cd39062edfb514994964305eb585
Carl Hammarling <carlhammarling@gmail.com>
2023-05-10 09:08:31 +0100

    Started working on nav

--- Watches/frontend/src/components/Navbar.jsx
+++ Watches/frontend/src/components/Navbar.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="left-nav">
+        <h1>Bmerketo</h1>
+      </div>
+      <div className="right-nav">
+        <ul className='nav-links'>
+          <li>HOME</li>
+          <li>PRODUCTS</li>
+          <li>CONTACT</li>
+          <li>SEARCH</li>
+          <li>LOGIN</li>
+          <li>CART</li>
+        </ul>
+
+      </div>
+    </div>
   )
 }
 
