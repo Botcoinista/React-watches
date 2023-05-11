1b5d1de0e1a2e58e5cc505caa2eaf8f47c6016c0
Carl Hammarling <carlhammarling@gmail.com>
2023-05-11 14:21:36 +0200

    Completed ProductDetails

--- Watches/frontend/src/components/FeaturedProducts.jsx
+++ Watches/frontend/src/components/FeaturedProducts.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const FeaturedProducts = () => {
   return (
-    <div>FeaturedProducts</div>
+    <div className='featuredProducts'>
+
+      <i class="fa-solid fa-angle-left fa-sm"></i>
+
+      <div className="wrapper">
+        <div className="imgDiv"></div>
+        <div className="imgDiv"></div>
+        <div className="imgDiv"></div>
+        <div className="imgDiv"></div>
+        <div className="imgDiv"></div>
+
+      </div>
+
+      <i class="fa-solid fa-chevron-right fa-sm"></i>
+
+    </div>
   )
 }
 
