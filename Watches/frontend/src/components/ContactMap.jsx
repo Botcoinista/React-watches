ef0be3d069625a4574e663d3744f902e60153be7
Josef <98840857+Botcoinista@users.noreply.github.com>
2023-05-11 11:49:50 +0200

    added some comments

--- Watches/frontend/src/components/ContactMap.jsx
+++ Watches/frontend/src/components/ContactMap.jsx
@@ -10,17 +10,18 @@ const mapContainerStyle = {
   marginTop: '100px',
 };
 
+// Latitude coordinate for the center of Stockholm
 const center = {
-  lat: 59.3293, // Latitude coordinate for the center of Stockholm
-  lng: 18.0686, // Longitude coordinate for the center of Stockholm
+  lat: 59.3293, 
+  lng: 18.0686,
 };
 
 
-// Define your API key
-// const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+// Defines your API key
 const apiKey = "AIzaSyA2CXz6RpZgSPmVZkdhJCB4HBJddPB2x44";
+// const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 
-// Create the Map component
+// Creates the Map component
 const ContactMap = () => {
   return (
     <LoadScript googleMapsApiKey={apiKey}>
