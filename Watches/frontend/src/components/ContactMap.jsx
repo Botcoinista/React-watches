6e544b1240c5ff79150482f0ab4b10981991af34
Shahriar Munir <smunir07@gmail.com>
2023-05-11 20:03:08 +0200

    Add selected product section

--- Watches/frontend/src/components/ContactMap.jsx
+++ Watches/frontend/src/components/ContactMap.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import React from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 // import dotenv from 'dotenv';
 // dotenv.config();
 
 // Set up the map options
 const mapContainerStyle = {
-  width: '100%',
-  height: '600px',
-  marginTop: '100px',
+  width: "100%",
+  height: "600px",
+  marginTop: "100px",
 };
 
 // Latitude coordinate for the center of Stockholm
 const center = {
-  lat: 59.3293, 
+  lat: 59.3293,
   lng: 18.0686,
 };
-
 
 // Defines your API key
 const apiKey = "AIzaSyA2CXz6RpZgSPmVZkdhJCB4HBJddPB2x44";
