06ded38d0d01843d8b30451fdcd1593b8ad9eec1
ulrikaha <98079099+ulrikaha@users.noreply.github.com>
2023-05-12 20:52:26 +0200

    added some more style to shoppingCart

--- Watches/frontend/src/components/ShoppingCart.jsx
+++ Watches/frontend/src/components/ShoppingCart.jsx
@@ -1,23 +1,35 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+
 function ShoppingCart() {
   return (
     <div className='shoppingCart'>
-      <div className="heroCart">        
+    <div className='heroCart'>        
         <h1>CART</h1>
         <ul>
           <li><NavLink to='/'>HOME.</NavLink> </li>
           <li>SHOPPING CART</li>
         </ul>
       </div>
-    
-</div>
-
   
-  
-
-  )
+      <div className='tableContainer'>
+      <table>
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* Add table rows for products */}
+          </tbody>
+      </table>
+    </div>
+    </div>
+  );
 }
 
-export default ShoppingCart
+export default ShoppingCart;
