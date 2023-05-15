3ea26860e2a644922c1b878c2e8147ce4f029459
Josef <98840857+Botcoinista@users.noreply.github.com>
2023-05-15 11:44:03 +0200

    changed back the login

--- Watches/backend/app.js
+++ Watches/backend/app.js
@@ -11,9 +11,9 @@ app.use(express.json());
 
 
 //Controllers
-app.use('/watch/products', require('./controllers/productController'));
-app.use('/watch/users', require('./controllers/userController'));
-app.use('/watch/orders', require('./controllers/orderController'));
+app.use('/api/products', require('./controllers/productController'));
+app.use('/api/users', require('./controllers/userController'));
+app.use('/api/orders', require('./controllers/orderController'));
 
 
 //Export app
