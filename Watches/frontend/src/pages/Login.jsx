9479433640b0cf2302a18e99dac1d1c4402bfabb
Carl Hammarling <carlhammarling@gmail.com>
2023-05-15 10:39:15 +0200

    connected

--- Watches/frontend/src/pages/Login.jsx
+++ Watches/frontend/src/pages/Login.jsx
@@ -4,7 +4,6 @@ import LoginForm from '../components/LoginForm'
 const Login = ({ user, setUser }) => {
   return (
     <>
-    <h1>hej</h1>
       <LoginForm user={user} setUser={setUser}/>
     </>
   )
