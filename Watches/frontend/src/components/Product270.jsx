3036852d6c5059fca9f9450904c8daee9c233479
Carl Hammarling <carlhammarling@gmail.com>
2023-05-15 10:15:53 +0200

    testa loopa

--- Watches/frontend/src/components/Product270.jsx
+++ Watches/frontend/src/components/Product270.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import { useState, useEffect, json } from 'react'
 import { Link } from 'react-router-dom'
 
-const Product270 = () => {
+const Product270 = ({ prod }) => {
+
+  
   return (
     <div className='product270'>
       <div className='productImg'></div>
       <div className='title'>
-        <Link>Daniel Wellington</Link>
+        <Link>{ prod.name }</Link>
       </div>
       <div className='price'>
-        <p>$500</p>
+        <p>{prod.price}</p>
       </div>
     </div>
   )
