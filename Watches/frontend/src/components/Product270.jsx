27fa60f71d24072d724db134cdc07094f656b95f
Carl Hammarling <carlhammarling@gmail.com>
2023-05-15 10:44:47 +0200

    change

--- Watches/frontend/src/components/Product270.jsx
+++ Watches/frontend/src/components/Product270.jsx
@@ -8,6 +8,7 @@ const Product270 = ({ prod }) => {
     <div className='product270'>
       <div className='productImg'></div>
       <div className='title'>
+        <h1>hej</h1>
         <Link>{ prod.name }</Link>
       </div>
       <div className='price'>
