b224abe02ee01ff954d50b1bf8c89cae427b0ea8
Josef <98840857+Botcoinista@users.noreply.github.com>
2023-05-24 15:12:31 +0200

    moved post orser to checkout

--- Watches/frontend/src/components/CheckOut.jsx
+++ Watches/frontend/src/components/CheckOut.jsx
@@ -1,58 +1,59 @@
-import React from 'react'
-import shoppingCart from '../components/shoppingCart'
-import { useSelector } from 'react-redux'
-
-
-//I store / features skapa en mapp/auth och en fil authSlice.js
-//import { createSlice } from '@reduxjs/toolkit'
-
-/*const initialState = {
-  user: null,
-  loading: false,
-  error: null
-}
-export const authSlice = createSlice({
-  name: 'auth',
-  initialState,
-  reducers: {},
-  extraReducers: {builder} => {}
-})
-
-export default authSlice.reducer
-*/
-
-//I store / index.js 
-//lägg till reducer, auth: authSlice
-//importera authSlice från authSlice.js
-
-
+import React from "react";
+import shoppingCart from "../components/shoppingCart";
+import { useSelector } from "react-redux";
+import axios from 'axios'
+import { useState, useEffect } from "react"
 
 const CheckOut = () => {
+  const token = localStorage.getItem("token");
 
+  const { cart } = useSelector((state) => state.cart);
+  //Structure of an order.
+  const [order, setOrder] = useState({
+    orderLines: [],
+  });
 
-//const { cart } = useSelector(state => state.shoppingCart)
-//const { user } = useSelector(state => state.auth)
+  //Transforming cart into order structure.
+  useEffect(() => {
+    const filteredCart = cart.map((item) => ({
+      product: item.product._id,
+      quantity: item.quantity,
+    }));
 
-//const placeOrder = () => {
-  // Map over the items in the cart and create a new array called order
-  //const order  = cart.map(item => {
-   // return { id: item.product.id, 
-   //quantity: item.quantity 
-  //}
-  //})
+    setOrder((prevOrder) => ({
+      ...prevOrder,
+      orderLines: filteredCart,
+    }));
+    console.log(order);
+  }, [cart]);
 
- 
+  const submitHandler = (e) => {
+    e.preventDefault();
 
+    const requestBody = order;
+
+    axios
+      .post("http://localhost:8080/api/orders", requestBody, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   return (
     <div className="checkOut">
       <div className="wrapper-1">
-        
-          {/* Cart summary */}
-          <h3 className="cart-header">Your cart contains</h3>
-          <br></br>
-          <span className="round">3</span>
-       
+        {/* Cart summary */}
+        <h3 className="cart-header">Your cart contains</h3>
+        <br></br>
+        <span className="round">3</span>
+
         <ul className="list-group mb-3">
           <li className="list-group-item">
             <div>
@@ -63,18 +64,16 @@ const CheckOut = () => {
           <li className="list-group-item">
             <div>
               <h5 className="h5">Second product</h5>
-              
             </div>
             <span className="price">$0</span>
           </li>
           <li className="list-group-item">
             <div>
               <h5 className="h5">Third item</h5>
-            
             </div>
             <span className="price">$0</span>
           </li>
-          
+
           <li className="list-group-item">
             <span>Total (USD)</span>
             <strong>200</strong>
@@ -82,35 +81,64 @@ const CheckOut = () => {
         </ul>
       </div>
 
-            {/* Payment */}
-            <div className="wrapper-2">
-          <form>
+      {/* Payment */}
+      <div className="wrapper-2">
+        <form>
           <h2 className="h2">Payment</h2>
           <hr></hr>
           <div className="row">
             <div className="col1">
-              <label htmlFor="cc-name" className="form-label">Name on card</label>
-              <input type="text" className="form-control" id="cc-name" required />
+              <label htmlFor="cc-name" className="form-label">
+                Name on card
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="cc-name"
+                required
+              />
             </div>
             <div className="col1">
-              <label htmlFor="cc-number" className="form-label">Credit card number</label>
-              <input type="text" className="form-control" id="cc-number" required />
+              <label htmlFor="cc-number" className="form-label">
+                Credit card number
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="cc-number"
+                required
+              />
             </div>
             <div className="col1">
-              <label htmlFor="cc-expiration" className="form-label">Expiration</label>
-              <input type="text" className="form-control" id="cc-expiration" required />
+              <label htmlFor="cc-expiration" className="form-label">
+                Expiration
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="cc-expiration"
+                required
+              />
             </div>
             <div className="col1">
-              <label htmlFor="cc-cvv" className="form-label">CVV</label>
-              <input type="text" className="form-control" id="cc-cvv" required />
+              <label htmlFor="cc-cvv" className="form-label">
+                CVV
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="cc-cvv"
+                required
+              />
             </div>
           </div>
 
-          <button className="btn-submit" type="submit">Place order</button>
-          </form>
+          <button className="btn-submit" type="submit" onClick={submitHandler}>
+            Place order
+          </button>
+        </form>
       </div>
-      </div>
-    
+    </div>
   );
 };
 
